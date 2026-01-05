1e0b04788886c462ffe4cbb7e207f6cf1e8c8117
Clébio Júnior <clebiojuniorcj@gmail.com>
2026-01-05 17:05:49 -0300

    refactori: rename functions for consistency in main.py

--- src/dstemplate/main.py
+++ src/dstemplate/main.py
@@ -4,9 +4,9 @@ from .utils import (
     create_readme_file,
     create_license_file,
     create_model_folder,
-    create_src_structure,
+    create_src_directory,
     create_reports_folder,
-    create_data_structure,
+    create_data_directory,
     create_notebooks_folder,
     create_reference_folder,
 )
@@ -15,13 +15,13 @@ from .utils import (
 def main(project_name: str) -> None:
     create_readme_file()
     create_license_file()
-    create_data_structure()
+    create_data_directory()
     create_model_folder()
     create_notebooks_folder()
     create_docs_folder()
     create_reports_folder()
     create_reference_folder()
-    create_src_structure(project_name)
+    create_src_directory(project_name)
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
